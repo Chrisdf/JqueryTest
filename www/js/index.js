3ee05ec0bb3c6b210d96a66031bd4ae48566b795
Chris Frantz <chrisfrantz96@gmail.com>
2015-08-15 11:28:23 -0700

    Added list that displays database files, testing bluetooth

--- www/js/index.js
+++ www/js/index.js
@@ -21,15 +21,10 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        console.log("initialized");
         var fdb = new ForerunnerDB();
         var db = fdb.db('OFSPRO');
         var collection = db.collection('work_orders');
         this.onDeviceReady();
-
-
-       
-		
     },
     // Bind Event Listeners
     //
@@ -45,7 +40,6 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        console.log("recieved device ready event");
         var deviceLocationElement = document.getElementById('device_location');
             var fdb = new ForerunnerDB();
             var db = fdb.db('ofs2');
@@ -53,14 +47,42 @@ var app = {
 
             numbers.insert([{ "_id": "55cce96b17e4bcdcaba6a6ce", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec117e4bcdcaba6a6cf", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec117e4bcdcaba6a6d0", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec117e4bcdcaba6a6d1", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec217e4bcdcaba6a6d2", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec217e4bcdcaba6a6d3", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec217e4bcdcaba6a6d4", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec217e4bcdcaba6a6d5", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec217e4bcdcaba6a6d6", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec317e4bcdcaba6a6d7", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec317e4bcdcaba6a6d8", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec317e4bcdcaba6a6d9", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec317e4bcdcaba6a6da", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceec417e4bcdcaba6a6db", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceecf17e4bcdcaba6a6dc", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceed017e4bcdcaba6a6dd", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceed017e4bcdcaba6a6de", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceed017e4bcdcaba6a6df", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceed017e4bcdcaba6a6e0", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceed017e4bcdcaba6a6e1", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceed117e4bcdcaba6a6e2", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceed117e4bcdcaba6a6e3", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceed117e4bcdcaba6a6e4", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceefd17e4bcdcaba6a6ec", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceefe17e4bcdcaba6a6ed", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceefe17e4bcdcaba6a6ee", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceefe17e4bcdcaba6a6ef", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55cceeff17e4bcdcaba6a6f0", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler" }, { "_id": "55ccf49117e4bcdcaba6a6f6", "work_order_no": "GJ241", "job_scope": "This is a description job scope", "supervisor": "James Stutler", "work_order_assets": [{ "_id": "55ccf49117e4bcdcaba6a6f5", "asset_id": "55ccf40a17e4bcdcaba6a6f4", "hours": 15.2 }] }]);
 
-            numbers.link('#work_order_list', '#myLinkFragment');
- 
-            var work_order_data = numbers.find({}, {});
+            var doc = db.document('testdoc');
+
+            db.collection('numbers').link('#work_order_list', {
+                template: '<ul>{^{for items}}<li>{^{:work_order_no}}: {^{:job_scope}}</li>{{/for}}</ul>'
+            }, {
+                $wrap: 'items',
+                $wrapIn: doc
+            });
+
+            doc.update({});
+
+
+            var starttime = performance.now();
+            numbers.insert({"work_order_no": "TESTING", "job_scope": "THIS IS A TESTING JOB", "description": "Hello I am a description meep" });
+            var endtime = performance.now();
+            var total = endtime-starttime;
+            console.log("TIME TO INSERT A RECORD: " + (total) + ".");
+
+            var startTime = performance.now();
+            var work_order_data = numbers.find({work_order_no: "TESTING"}, {});
+            var endTime = performance.now();
+            var Total = endTime - startTime;
+            console.log("TIME TO FIND A RECORD: " + (Total) + ".");
+
+            console.log(work_order_data[0]["job_scope"]);
+
             var workOrderList = document.getElementById('work_order_list');
-                    
-            var name = workOrderList.innerHTML;
-            console.log(name);
-        },
+
+
+            var bluetoothDevices = bluetoothSerial.list(function(devices) {
+                devices.forEach(function(device) {
+                    console.log(device.address);
+                })
+            }, failure);
+
+            },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -71,6 +93,5 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
     }
 };
